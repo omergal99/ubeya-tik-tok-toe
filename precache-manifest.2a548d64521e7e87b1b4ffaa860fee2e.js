95e8b380a5c31879b90dec843be522b84b07881b
omergal99 <omergal99gm@gmail.com>
2020-08-16 19:19:18 +0300

    Updates

---
+++ precache-manifest.2a548d64521e7e87b1b4ffaa860fee2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e22627d4717db0076a88f10b1a09624",
+    "url": "/ubeya-tik-tok-toe/index.html"
+  },
+  {
+    "revision": "2383cd90b19ed02885af",
+    "url": "/ubeya-tik-tok-toe/static/css/main.cf1778c7.chunk.css"
+  },
+  {
+    "revision": "a6618719477e4e51c0b4",
+    "url": "/ubeya-tik-tok-toe/static/js/2.3b139c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ubeya-tik-tok-toe/static/js/2.3b139c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2383cd90b19ed02885af",
+    "url": "/ubeya-tik-tok-toe/static/js/main.2856673e.chunk.js"
+  },
+  {
+    "revision": "ca2eb67d8f2766c83d84",
+    "url": "/ubeya-tik-tok-toe/static/js/runtime-main.2513002c.js"
+  }
+]);
